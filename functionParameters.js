24564c0dea4bc73543f488cd9c6b6595d3e72fba
Soujanya RG <soujanyasoujzzzz@gmail.com>
2023-06-09 15:46:46 +0530

    challenge 8 check presence of function parameters is added

---
+++ functionParameters.js
@@ -0,0 +1,27 @@
+/* CHALLENGE 8 - Check presence of the function parameters
+
+Throw Error when function square() is called
+without arguments.
+
+Create new function and use it as default parameter.
+*/
+
+"use strict";
+
+// function square(a) {
+//   console.log(a * a);
+// }
+
+square(10);
+// 100
+
+square();
+// BEFORE: NaN
+// AFTER: Uncaught Error: Function square requires
+// an argument!
+
+//-----------Solution------------
+
+function square(a=0) {
+    console.log(a * a);
+  }
